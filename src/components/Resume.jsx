58f6fb93bcd5489dbc92f5bafca90e4347d336c5
donnacancode <donnacancode@gmail.com>
2024-10-16 16:45:12 -0400

    update resume to png, style tweaks

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,285 +1,41 @@
 import React from "react";
+import Resume1 from "../assets/graphics/Resume1.png"; // Correct import for files in src/assets
+import Resume2 from "../assets/graphics/Resume2.png";
 
 const Resume = () => {
   return (
     <section className="resume-container">
       <div className="resume-card">
-        <h2>Donna Burns</h2>
-        <p>
-          Highlands, NJ
-          <br />
-          732.347.4297
-          <br />
-          <a href="mailto:DonnaCanCode@gmail.com" style={{ color: "#81d8d0" }}>
-            DonnaCanCode@gmail.com
-          </a>
-        </p>
+        <h2>My Resume</h2>
+        {/* Center the images by adding styling */}
+        <img
+          src={Resume1}
+          alt="Resume Page 1"
+          style={{
+            display: "block",
+            margin: "0 auto 20px",
+            borderRadius: "10px",
+            width: "75%",
+          }}
+        />
+        <img
+          src={Resume2}
+          alt="Resume Page 2"
+          style={{
+            display: "block",
+            margin: "0 auto 20px",
+            borderRadius: "10px",
+            width: "75%",
+          }}
+        />
 
-        <h3>Experience</h3>
-        <div className="resume-content">
-          <h3>Ross Brewing Company, Belford, NJ</h3>
-          <p>
-            <strong>Admin and Events Manager</strong> (August 2020 – April 2024)
-          </p>
-          <ul>
-            <li>
-              Develop and coordinate exciting weekly events that drive customer
-              growth.
-            </li>
-            <li>
-              Design and strategize large-scale festivals designed to expand
-              brand recognition while safely enjoying alcoholic beverages.
-            </li>
-            <li>
-              Build a stable revenue stream of private parties and events.
-            </li>
-            <li>
-              Effectively engage in proactive communication and collaboration
-              with superiors to ensure alignment and mutual understanding.
-            </li>
-            <li>
-              Coordinate with local bands and vendors for unique on-site
-              entertainment.
-            </li>
-            <li>
-              Schedule and staff external events such as tastings, festivals,
-              and local vending opportunities.
-            </li>
-            <li>
-              Ensure the brewery operates in compliance with local state and
-              federal regulations related to alcohol production, health and
-              safety, environmental standards, and other relevant laws.
-            </li>
-            <li>
-              Created and implemented all tasting room policies and procedures
-              for location opening in April 2023.
-            </li>
-            <li>Hire, train, and manage brewery taproom managers and staff.</li>
-            <li>
-              Manage taproom staffing schedule to minimize payroll expenses
-              while maximizing total revenue.
-            </li>
-            <li>Acquired proficiency in payroll management.</li>
-            <li>
-              Demonstrate quick and adaptive thinking in dynamic and rapidly
-              changing situations.
-            </li>
-            <li>
-              Adeptly mediate and facilitate the resolution of interpersonal
-              conflicts among team members.
-            </li>
-          </ul>
-          <br />
-
-          <h3>Shore Thing Pet Concierge LLC, Highlands, NJ</h3>
-          <p>
-            <strong>Owner</strong> (January 2020 – Present)
-          </p>
-          <ul>
-            <li>
-              Founded and manage a thriving pet care business, providing
-              exceptional care and services to clients' beloved pets.
-            </li>
-            <li>
-              Demonstrate strong organizational skills by scheduling and
-              coordinating pet care services, ensuring seamless operations and
-              optimal client satisfaction.
-            </li>
-            <li>
-              Built a loyal and satisfied customer base through attentive and
-              personalized pet care, resulting in repeat business and positive
-              referrals.
-            </li>
-            <li>
-              Utilize effective communication to establish rapport with clients
-              and understand their specific needs, resulting in tailored and
-              high-quality pet care services.
-            </li>
-            <li>
-              Implement safety protocols and maintain a clean and hygienic
-              environment, ensuring the well-being and health of all pets under
-              care.
-            </li>
-            <li>
-              Leverage marketing strategies, including social media presence and
-              word-of-mouth recommendations, to promote the business and attract
-              new clients.
-            </li>
-            <li>
-              Demonstrate strong problem-solving abilities by addressing various
-              challenges and unexpected situations in a calm and efficient
-              manner.
-            </li>
-          </ul>
-          <br />
-
-          <h3>Roosevelt Island Operating Corporation, New York, NY</h3>
-          <p>
-            <strong>Program Manager</strong> (April 2014 – August 2019)
-          </p>
-          <ul>
-            <li>
-              Responsible for continuous planning, scheduling, and management of
-              recurring and single-event activities including non-profit events,
-              youth programs, religious events, sports leagues, 5K/10K races,
-              film shoots, and construction work.
-            </li>
-            <li>
-              Primary point person for accepting and approving permit
-              applications of residents, as well as public and private
-              organizations to ensure compliance with Standard Operating
-              Procedures and governing laws and regulations.
-            </li>
-            <li>
-              Responsible for overseeing public use of four multi-sport fields,
-              recreation center with pool and multipurpose areas, two community
-              centers, all open park space, and barbecue areas.
-            </li>
-            <li>
-              Presenting at public meetings to address upcoming events and
-              public matters or concerns related to such.
-            </li>
-            <li>
-              Multi-tasking and coordinating with various inter-organizational
-              departments including local law enforcement for events occurring
-              on Roosevelt Island to ensure public safety.
-            </li>
-            <li>
-              Leading and conducting effective internal meetings on a weekly
-              basis with the local law enforcement, Capital Planning Department,
-              and Executive team for recurring and non-recurring permitted
-              events.
-            </li>
-            <li>
-              Project Management including implementation of a customer-facing
-              permitting portal 'Civic Permits'—an online scheduling and payment
-              portal that empowers internal and external customers to book
-              available facilities.
-            </li>
-            <li>
-              Continuously evaluating and implementing revised Standard
-              Operating Procedures for the Parks and Recreation Department,
-              which are currently being used as a template for the entire
-              Roosevelt Island organization.
-            </li>
-            <li>
-              Managing support staff which included scheduling of staff,
-              conducting performance appraisals, and ongoing coaching/mentoring
-              to ensure optimal and enthusiastic performance.
-            </li>
-          </ul>
-
-          <p>
-            <strong>Program Coordinator</strong> (January 2007 – April 2014)
-          </p>
-          <ul>
-            <li>
-              Exclusively handled all aspects of marketing, scheduling,
-              permitting, accounts payable, and receivable for all facilities
-              and open spaces on Roosevelt Island.
-            </li>
-            <li>
-              Worked closely with grounds and facilities crews to ensure
-              exemplary client accommodations.
-            </li>
-            <li>
-              Streamlined tennis court access control, membership protocol for
-              500+ members.
-            </li>
-            <li>
-              Maintained daily contact with clients to ensure carefully
-              coordinated and successful events.
-            </li>
-            <li>
-              Redesigned Parks and Recreation portion of the agency website to
-              be more user-friendly.
-            </li>
-            <li>
-              Established the permitting area as a key revenue stream and was
-              granted additional staff to turn the area into a “department”.
-            </li>
-          </ul>
-          <br />
-
-          <h3>Equinox Fitness, NYC, New York, NY</h3>
-          <p>
-            <strong>Membership Advisor</strong> (June 2006 – December 2006)
-          </p>
-          <ul>
-            <li>
-              September 2006—finished seventh nationally in sales percentage.
-            </li>
-            <li>Achieved 110% Elite Sales Status third quarter 2006.</li>
-            <li>
-              Provided potential new members with tours and various membership
-              options.
-            </li>
-            <li>
-              Ensured highest quality experience for members by meeting and
-              exceeding their requests.
-            </li>
-          </ul>
-          <br />
-
-          <h3>Chelsea Piers, NYC, New York, NY</h3>
-          <p>
-            <strong>Operations Manager</strong> (December 2005 – June 2006)
-          </p>
-          <ul>
-            <li>
-              Improvised solutions to both routine and unusual problems that
-              occurred.
-            </li>
-            <li>Planned and implemented new staff orientation and training.</li>
-            <li>
-              Primary monitor of 150,000 square foot facility for safety and
-              efficiency.
-            </li>
-            <li>
-              Developed new recreational programs for current and prospective
-              members.
-            </li>
-            <li>
-              Interacted with members and guests to ensure customer
-              satisfaction.
-            </li>
-          </ul>
-          <br />
-
-          <h3>Rutgers College Recreation, Piscataway, NJ</h3>
-          <p>
-            <strong>Building Manager</strong> (September 2001 – December 2005)
-          </p>
-          <ul>
-            <li>
-              Monitored staff and inspected center regularly for
-              structural/safety concerns.
-            </li>
-            <li>
-              Primary liaison between facility patrons and upper management.
-            </li>
-            <li>
-              Assisted in creating and preparing for special events taking place
-              at the facility.
-            </li>
-            <li>
-              Coordinated team-building exercises for all student and
-              professional staff.
-            </li>
-          </ul>
-        </div>
-
-        <h3>Education</h3>
-        <p>
-          <strong>
-            Rutgers College, Rutgers University, New Brunswick, NJ
-          </strong>
-        </p>
-        <p>Bachelor of Arts</p>
-
-        <div className="download-resume">
+        {/* Download button for the full PDF version */}
+        <div
+          className="download-resume"
+          style={{ marginTop: "20px", textAlign: "center" }}
+        >
           <a
-            href="./assets/DonnaBurnsResume.pdf"
+            href="/DonnaBurnsResume.pdf"
             download="Donna_Burns_Resume.pdf"
             className="download-button"
           >
